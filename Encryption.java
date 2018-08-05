9489c439daa3f52bea48b8f0e4c23e914d63e664
Bobarilla <thisismyjunkmailabc@gmail.com>
2018-08-05 03:32:32 -0400

    changed Encryption to work with ASCII

--- Encryption.java
+++ Encryption.java
@@ -1,5 +1,9 @@
 import java.io.*;
 import java.util.Scanner;
+/*
+26 letters in the alphabet
+A=shift 0, Z=shift 25
+*/
 public class Encryption {
 	public static void main(String[] args) throws IOException {
 		//FileWriter write = new FileWriter("./txt/encrypted.txt", true);
@@ -7,34 +11,23 @@ public class Encryption {
 		FileWriter dec = new FileWriter("./txt/decrypted.txt", true);//decryptor
 		char[] msg={}, key, keyMsg, encr;
 		int ln1=1, ln2=2;
-		//.write("hello world!");
-		//.flush();
-		//.close();
-
-		//  Message
-		//+ KeyMess
-		//---------
-		//  Encrypt(ion) & Key
-		
 		clearFile("./txt/encrypted.txt");
 		clearFile("./txt/decrypted.txt");
+
 		System.out.println("\n./txt/unencrypted.txt");
 		Tools.PrintFile("./txt/unencrypted.txt");
+
 		while (true) {
 			try {
-				msg = readMsgLn("./txt/unencrypted.txt", ln1);
+				msg = toCharArr(Tools.ReadLn("./txt/unencrypted.txt", ln1));
 			} catch (java.util.NoSuchElementException e) {
 				break;
 			}
-			key = readMsgLn("./txt/unencrypted.txt", ln2);
-			keyMsg = new char[msg.length];
-			//System.out.println(keyMsg.length);
-			//System.out.println(key.length);
-			//System.out.println(msg.length);
+			key = toCharArr(Tools.ReadLn("./txt/unencrypted.txt", ln2));
+			keyMsg = new char[msg.length];//create keMmsg
 			for (int i=0; i<key.length; i++) {
 				keyMsg[i]=key[i];
 			}
-			//System.out.println((msg.length-key.length));
 			for (int i=key.length; i<msg.length; i++) {
 				keyMsg[i]=msg[i-key.length];
 			}
@@ -58,51 +51,56 @@ public class Encryption {
 		Tools.PrintFile("./txt/encrypted.txt");
 		while (true) {
 			try {
-				encr = readMsgLn("./txt/encrypted.txt", ln1);
+				encr = toCharArr(Tools.ReadLn("./txt/encrypted.txt", ln1));
 			} catch (java.util.NoSuchElementException e) {
 				break;
 			}
-			key = readMsgLn("./txt/encrypted.txt", ln2);
-			msg = decrypt(encr, key);
-
+			key = toCharArr(Tools.ReadLn("./txt/encrypted.txt", ln2));
+			msg = decrypt(encr, key);//decrypt message
+			for (char c : msg) {//write to decrypted.txt
+				dec.write(c);
+			}
+			dec.write("\n");
+			for (char c : key) {
+				dec.write(c);
+			}
+			dec.write("\n");
 			ln1+=2;
 			ln2+=2;
 		}
 		dec.flush();
 		dec.close();
+		System.out.println("\n./txt/decrypted.txt");
+		Tools.PrintFile("./txt/decrypted.txt");
 	}
-	
-	static char[] readMsgLn(String filePath, int lnNum) throws IOException {
+
+	static char[] toCharArr(String ln) throws IOException {
 		//Read a line as char[]
-		Scanner read = new Scanner(new FileReader(filePath));
-		String ln="";
-		for (int i=0; i<lnNum; i++) {
-			ln = read.nextLine();
-		}
-		//System.out.println(ln);
 		char[] msg = new char[ln.length()];
-		lnNum=0;
+		int i=0;
 		for (char ch : msg) {
-			msg[lnNum]=ln.charAt(lnNum);
-			lnNum++;
+			msg[i]=ln.charAt(i);
+			i++;
 		}
 		return msg;
 	}
 
 	static char[] encrypt(char[] msg, char[] keyMsg) {
-		char[] alf = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
 		int m;
 		int km;
 		for (int i=0; i<msg.length; i++) {
-			m=msg[i]-65; //got number equivalent 1-26
-			km=keyMsg[i]-65;
-			msg[i]=alf[m+km];
+			m=(int) msg[i]; //get msg[i]'s ASCII Decimal Value from 65-90
+			km=(int) keyMsg[i]-65; //get shift from 0-25;
+			if (m+km>90) {
+				msg[i]=(char) (m+km-26);
+			} else {
+				msg[i]=(char) (m+km);
+			}
 		}
 		return msg;
 	}
 
 	static char[] decrypt(char[] encr, char[] key) {
-		char[] alf = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
 		// 3
 		// encrencrencr
 		//-keymsg0msg1msg2
@@ -111,12 +109,17 @@ public class Encryption {
 		// not mult of 3: remander of: 1 or 2
 		int e, km, m, r=encr.length%3;
 		for (int i=0; i<encr.length; i++) {
-			e=encr[i]-65;
-			km=key[i]-65;
-			encr[i]=alf[e-km];
+			e=(int) encr[i]; //get encr[i] ASCII Decimal Value from 65-90
+			km=(int) key[i]-65; //get shift from 0-25
+			if (e-km<65) {
+				encr[i]=(char) (25+e-km);//(90-(65-(e-km))) = (25+e-km)
+			}
+			encr[i]=(char) (e-km);
 			if ((i+1)%3==0) {
 				for (int j=0; j<3; j++) { //i-3??
+					//i-=2;
 					key[j]=encr[i];
+					//i+=2;
 				}
 			}
 		}
--- Tools.java
+++ Tools.java
@@ -1,12 +1,14 @@
 import java.io.*;
 import java.util.Scanner;
 public class Tools {
+	//IN PROGRESS
 	static void PrintLns(String filePath, int lnStart, int lnEnd) throws IOException {
 		Scanner read = new Scanner(new FileReader(filePath));
 		for (int i=0; i<lnStart; i++) {
 			read.nextLine();
 		}
 	}
+
 	static void PrintFile(String filePath) throws IOException {
 		Scanner read = new Scanner(new FileReader(filePath));
 		while (true) {
@@ -17,5 +19,13 @@ public class Tools {
 				break;
 			}
 		}
+	}
+	static String ReadLn(String filePath, int lnNum) throws IOException {
+		Scanner read = new Scanner(new FileReader(filePath));
+		String ln="";
+		for (int i=1; i<lnNum; i++) {
+			ln = read.nextLine();
+		}
+		return read.nextLine();
 	}
 }
